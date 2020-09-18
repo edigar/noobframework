afdb52a068522ecc0bc99ac3f21fb67fa48d79f8
print('helloworld') <maxwellmacedo2015@gmail.com>
2020-09-18 13:50:13 -0300

    Fix/Feature: Fix #1 - Fixed 'the method View only works with HTML files.'. Fix #2 - Fixed 'Variables's name inside the view was the same as the view name.'. Featu re #1 - New index page design.

--- Core/Controller.php
+++ Core/Controller.php
@@ -5,8 +5,24 @@ namespace Core;
 class Controller {
     
     protected function view($name, $params = null) {
-        $varName = $name;
-        $$varName = $params;
-        return require_once('App/views/' . $name . '.html');
+        $extName = '.html';
+
+        if ($params) {
+            $arrKeys = array_keys($params);
+    
+            foreach($arrKeys as $value) {
+                $$value = $params[$value];
+            }
+        }
+
+        if (file_exists('App/views/' . $name . '.html')) {
+            $extName = '.html';
+        } else if (file_exists('App/views/' . $name . '.phtml')) {
+            $extName = '.phtml';
+        } else if (file_exists('App/views/' . $name . '.php')) {
+            $extName = '.php';
+        }
+
+        return require_once('App/views/' . $name . $extName);
     }
 }
