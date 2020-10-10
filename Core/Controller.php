4111760f752736c906aa0b6bbd56767a20371c8f
Edigar Herculano <edigarhdev@gmail.com>
2020-10-10 19:19:45 -0300

    Add getConfig and redirect to class controller

--- Core/Controller.php
+++ Core/Controller.php
@@ -4,6 +4,21 @@ namespace Core;
 
 class Controller {
     
+    private $config = [];
+
+    public function __construct () {
+        global $config;
+        $this->config = $config;
+    }
+
+    protected function getConfig($config = null) {
+        if($config != null) {
+            return $this->config[$config];
+        }
+
+        return $this->config;
+    }
+    
     protected function view($name, $params = null) {
 
         if ($params) {
@@ -23,5 +38,15 @@ class Controller {
         }
 
         throw new \Exception('view not found');
+    }
+
+    protected function redirect($url, $statusCode = null) {
+        if($statusCode == null) {
+            header('Location: ' . $url);
+        } else {
+            header('Location: ' . $url, true, $statusCode);
+        }
+        
+        die();
     }
 }
