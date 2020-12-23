7e6f84dbe10e0762f84da4b70bd5764cae395545
Edigar Herculano <edigarhdev@gmail.com>
2020-12-23 09:39:53 -0300

    Add annotations to Controller.php

--- core/Controller.php
+++ core/Controller.php
@@ -4,7 +4,14 @@ namespace Core;
 
 class Controller {
 
-    protected function getConfig(string $item = null): array {
+    /**
+     * Get configs (one or all) on cofig/cofig.php
+     * 
+     * @param string $item config index wanted (optional)
+     * 
+     * @return mixed Value of config
+     */
+    protected function getConfig(string $item = null): mixed {
         global $config;
 
         if($item != null) {
@@ -14,6 +21,14 @@ class Controller {
         return $config;
     }
     
+    /**
+     * Load view
+     * 
+     * @param string    $name   view name
+     * @param array     $params params for view (optional)
+     * 
+     * @return string view
+     */
     protected function view(string $name, array $params = null): string {
         if ($params) {
             $arrKeys = array_keys($params);
@@ -34,6 +49,14 @@ class Controller {
         throw new \Exception('view not found');
     }
 
+     /**
+     * Redirects to provided url
+     * 
+     * @param string    $url
+     * @param array     $statusCode Redirect status code (optional)
+     * 
+     * @return void
+     */
     protected function redirect(string $url, int $statusCode = null): void {
         $urlBase = $this->getConfig('urlBase') == null ? "" : $this->getConfig('urlBase') . "/";
 
