cc61a1942363a41e6f2772e47afe5dd45d81bf37
Edigar Herculano <edigarhdev@gmail.com>
2020-12-28 17:22:25 -0300

    Add annotations to Request.php

--- core/Request.php
+++ core/Request.php
@@ -10,38 +10,88 @@ class Request {
         $this->params = $params;
     }
 
-    public function getParams() {
+    /**
+     * Get parameters sent by GET or POST
+     * 
+     * @return array parameters
+     */
+    public function getParams(): array {
         return $this->params;
     }
 
+    /**
+     * Get request method
+     * 
+     * @return string method
+     */
     public function method(): string  {
         return $_SERVER['REQUEST_METHOD'];
     }
 
+    /**
+     * Checks whether the request method is the one specified
+     * 
+     * @param string $type Request method to be checked
+     * 
+     * @return bool Whether or not the method is specified
+     */
     public function isMethod(string $type): bool {
         return $this->method() === strtoupper($type);
     }
 
+    /**
+     * Get client IP
+     * 
+     * @return string ip
+     */
     public function ip(): string {
         return isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
     }
 
+    /**
+     * Get URI
+     * 
+     * @return string uri
+     */
     public function path(): string {
         return $_SERVER['REQUEST_URI'];
     }
 
+    /**
+     * Request is using SSL?
+     * 
+     * @return bool
+     */
     public function ssl(): bool{
         return !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on';
     }
 
+    /**
+     * Get full path URL
+     * 
+     * @return string URL
+     */
     public function fullPath(): string {
         return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     }
 
+    /**
+     * Get params from GET method
+     * 
+     * @return mixed Get params
+     */
     public function get() {
         return $this->params[0];
     }
 
+    /**
+     * Get params (one or all) from POST method
+     * 
+     * @param string    $inputName  An especific parameter (optional)
+     * @param mixed     $default    If parameter not found, this will be returned (optional)
+     * 
+     * @return mixed Post params
+     */
     public function post(string $inputName = null, $default = null) {
         if($this->isMethod('POST')) {
             if($inputName !== null) return isset($this->params[1][$inputName]) ? $this->params[1][$inputName] : $default;
