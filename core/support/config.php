bdde3fa00434faa31d0b2343fff952c0fa9d6893
Edigar Herculano <edigarhdev@gmail.com>
2021-03-17 15:11:43 -0300

    Add configuration access function

---
+++ core/support/config.php
@@ -0,0 +1,20 @@
+<?php
+
+$config = file_exists('config/config.php') && is_file('config/config.php') ? include_once 'config/config.php' : [];
+
+/**
+ * Get configs (one or all) on config/config.php
+ *
+ * @param string|null $item Config index wanted (optional)
+ *
+ * @return mixed Value of config
+ */
+function config(string $item = null) {
+    global $config;
+
+    if($item != null) {
+        return $config[$item];
+    }
+
+    return $config;
+}
