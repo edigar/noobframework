17481859d61c145bf129f663765f3924dde3551c
Edigar Herculano <edigarhdev@gmail.com>
2024-02-08 15:22:22 -0300

    fix: config function item param can´t be optional (#12)

--- core/support/config.php
+++ core/support/config.php
@@ -3,11 +3,11 @@
 /**
  * Get environment variable value
  *
- * @param string|null $item config item wanted (optional)
+ * @param string $item config item wanted
  * @param mixed $default default value if item isn't set (optional)
  *
  * @return mixed Value of config
  */
-function config(string $item = null, mixed $default = null): mixed {
+function config(string $item, mixed $default = null): mixed {
     return $_ENV[$item] ?? $default;
 }
